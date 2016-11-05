218c2b17f2cef10136efbe766e52c5af06c4c772
Carol St.Louis <stlouis_c@yahoo.com>
2016-11-04 22:45:16 -0700

    imported Letter Component; passed data to it; displayed it;

--- src/Word.js
+++ src/Word.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Letter from './Letter';
 import './Word.css';
 
 const Word = ({word, lettersPlayed}) => {
+	const letters = word.split("");
 	return(
 		<div className="word">
-			<p>word: {word}</p>
-			<p>letters played: {lettersPlayed}</p>
+			{
+				letters.map((letter, i) => <Letter key={i} letter={letter} lettersPlayed={lettersPlayed} />)
+			}
 		</div>
 	)
 };
