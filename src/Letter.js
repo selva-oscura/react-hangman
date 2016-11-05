8b02ee7f7c9c2e4ac848bbd83db672860a38d66e
Carol St.Louis <stlouis_c@yahoo.com>
2016-11-04 23:36:15 -0700

    added color to letter and to underscore for all letters

--- src/Letter.js
+++ src/Letter.js
@@ -3,13 +3,14 @@ import './Letter.css';
 
 const Letter = ({letter, i, lettersPlayed, wordLength}) => {
 	let displayedLetter;
+	let color = Math.floor((i/wordLength)*360);
 	if(lettersPlayed.indexOf(letter)>=0){
 		displayedLetter = letter;
 	}else{
 		displayedLetter = " ";
 	}
 	return (
-		<div className="letter">
+		<div className="letter" style={{color: `hsl(${color}, 100%, 66%)`, borderBottom: `4px solid hsl(${color}, 100%, 66%)`}} >
 			<p>{displayedLetter}</p>
 		</div>
 	)
