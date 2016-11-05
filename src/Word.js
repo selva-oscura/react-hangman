550e14cb84299b1162a51392a2554995f2e2f668
Carol St.Louis <stlouis_c@yahoo.com>
2016-11-04 23:37:50 -0700

    added wordLength to data passed to Letter Component

--- src/Word.js
+++ src/Word.js
@@ -4,10 +4,11 @@ import './Word.css';
 
 const Word = ({word, lettersPlayed}) => {
 	const letters = word.split("");
+	const wordLength = word.length;
 	return(
 		<div className="word">
 			{
-				letters.map((letter, i) => <Letter key={i} letter={letter} lettersPlayed={lettersPlayed} />)
+				letters.map((letter, i) => <Letter key={i} letter={letter} i={i} lettersPlayed={lettersPlayed} wordLength={wordLength} />)
 			}
 		</div>
 	)
