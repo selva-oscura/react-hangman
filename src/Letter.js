8cc533e75696585e80045aa137ae68e83715d45f
Carol St.Louis <stlouis_c@yahoo.com>
2016-11-04 23:34:45 -0700

    made display of letter contingent upon it being in letterPlayed (all letters selected by player)

--- src/Letter.js
+++ src/Letter.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import './Letter.css';
 
-const Letter = ({letter,lettersPlayed}) => (
-	<div className="letter">{letter} {lettersPlayed}</div>
-);
+const Letter = ({letter, i, lettersPlayed, wordLength}) => {
+	let displayedLetter;
+	if(lettersPlayed.indexOf(letter)>=0){
+		displayedLetter = letter;
+	}else{
+		displayedLetter = " ";
+	}
+	return (
+		<div className="letter">
+			<p>{displayedLetter}</p>
+		</div>
+	)
+};
 
 export default Letter;
