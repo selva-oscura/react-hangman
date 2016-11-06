93115c91b36ea1840a0946022267de145a553455
Carol St.Louis <stlouis_c@yahoo.com>
2016-11-05 21:23:57 -0700

    imported and displayed LetterForm Component; created newLetter function to update lettersPicked and (if necessary) number of wrong letters in state

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import data from '../public/words.json';
 import Gallows from './Gallows';
 import Header from './Header';
-import Word from './Word';
 import LettersPicked from './LettersPicked';
+import Word from './Word';
+import LetterForm from './LetterForm';
+
 
 const App = React.createClass({
   getInitialState(){
@@ -14,12 +16,19 @@ const App = React.createClass({
         player:0,
       },
       // word: undefined,
-      // lettersPicked:[],
       word: data.words[2],
-      lettersPicked:["a", "b", "c"],
-      wrongLetters: 1,
+      lettersPicked:[],
+      wrongLetters: 0,
       message:"start",
     }
+  },
+  newLetter(letter){
+    let state = this.state;
+    state.lettersPicked.push(letter);
+    if(state.word.indexOf(letter)<0){
+      state.wrongLetters += 1;
+    }
+    this.setState(state);
   },
   render() {
     console.log('words', data.words);
@@ -31,13 +40,16 @@ const App = React.createClass({
         <Gallows 
           wrongLetters={this.state.wrongLetters}
         />
+        <LettersPicked  
+          lettersPicked={this.state.lettersPicked}
+        />        
         <Word 
           word={this.state.word}
           lettersPicked={this.state.lettersPicked}
         />
-        <LettersPicked  
-          lettersPicked={this.state.lettersPicked}
-        />        
+        <LetterForm 
+          newLetter={this.newLetter}
+        />
       </div>
     );
   }
