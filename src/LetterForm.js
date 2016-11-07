f65c13fc2b5b85faa1b0d8ea5d22ffb912b3fdc1
Carol St.Louis <stlouis_c@yahoo.com>
2016-11-06 19:37:07 -0800

    changed defaultValue to value for input and added state.defaultLetter to allow display and reset of input letter for LetterForm

--- src/LetterForm.js
+++ src/LetterForm.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './LetterForm.css';
 
-const LetterForm = ({newLetter}) => (
+const LetterForm = ({defaultLetter, newLetter}) => (
 	<div className="letterForm">
 		<input 
 			name="nextLetter" 
 			type="text" 
+			value={defaultLetter}
 			onChange={(e)=>{
 				console.log('e.nativeEvent.target.value', e.nativeEvent.target.value)
 				let inputValue = e.nativeEvent.target.value;
 				newLetter(inputValue.toLowerCase());
-	    	e.nativeEvent.target.value=""
 			}}
 		/>
 	</div>
