80fbe23b336dbba8025d2ab34bab794285a0ce01
Carol St.Louis <stlouis_c@yahoo.com>
2016-11-11 11:03:25 -0800

    created LettersSelection Component to display all letters for selection (replacement for LetterForm because keyboards on mobiles and forms low on a page are awkward)

---
+++ src/LettersSelection.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import LetterSelection from './LetterSelection';
+
+const LettersSelection = ({availableLetters, selectLetter}) => {
+	let letters = availableLetters.map((availableLetter) => (
+		<LetterSelection 
+			availableLetter={availableLetter}
+			selectLetter={selectLetter} 
+		/>
+	));
+	return(
+		<div className="lettersSelection">
+			{letters}
+		</div>
+	)
+}
+
+export default LettersSelection;
+
